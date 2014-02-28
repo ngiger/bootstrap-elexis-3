3225b162db276b333760d0f910612078478dca6f
Niklaus Giger <niklaus.giger@member.fsf.org>
2014-02-28 21:25:54 +0100

    Transformed bash into ruby script

---
+++ bootstrap_elexis_3_env.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+require 'open-uri'
+
+# configure to your liking
+
+ROOT				=	File.expand_path(File.dirname(__FILE__))
+CHECKOUT		=	File.join(ROOT, 'checkout')
+INSTALL_DIR	=	File.join(ROOT, 'eclipse')
+INSTALL_CMD	=	File.join(ROOT, 'director', 'director') + " -destination #{INSTALL_DIR} -profile Elexis "
+IUS="epp.package.rcp,com.inventage.tools.versiontiger,org.eclipse.m2e.sdk.feature.feature.group,com.wdev91.eclipse.copyright,com.google.eclipse.mechanic/0.3.4"
+REPOS="http://download.eclipse.org/releases/kepler,http://download.eclipse.org/technology/m2e/releases,https://raw.github.com/inventage/version-tiger-repos/master/releases,http://www.wdev91.com/update,http://workspacemechanic.eclipselabs.org.codespot.com/git.update/mechanic"
+JINTO_VERS = '0.13.5'
+
+# end of configuration section
+
+puts "#{Time.now}: Using #{CHECKOUT}/director, #{CHECKOUT} and #{INSTALL_DIR} to install #{IUS}"
+def system(cmd)
+	puts "#{Time.now}: running #{cmd}"
+	res = Kernel.system(cmd)
+	unless res
+		puts "#{Time.now}: FAILED running #{cmd}"
+		exit 1
+	end
+	res
+end
+
+def get_file_from_url(url, dest= File.basename(url))
+	open(url) {
+		|f|
+		File.open(File.basename(url), 'w+') { |outfile| outfile.write f.read }
+	}
+	puts "dest ist #{dest} size #{File.size(dest)} from url #{url}"
+end
+
+unless File.directory?(File.join(ROOT, 'director'))
+	Dir.chdir(ROOT)
+	FileUtils.rm_rf(["director", "director*.zip"])
+	exit 1 unless get_file_from_url("http://mirror.switch.ch/eclipse/tools/buckminster/products/director_latest.zip")
+	exit 1 unless system("unzip director_latest.zip")
+end
+
+FileUtils.makedirs(CHECKOUT)
+['elexis-3-base', 'elexis-3-core'].each {
+	|project|
+	unless File.directory?(File.join(CHECKOUT, project))
+		exit 1 unless system("git clone https://github.com/elexis/#{project}.git #{File.join(CHECKOUT, project)}")
+	end
+}
+
+mech_dest  = File.join('~', '.eclipse', 'mechanic')
+mech_tasks = Dir.glob(File.join(ROOT, 'mechanic', '*.epf'))
+FileUtils.makedirs(mech_dest)
+FileUtils.cp(mech_tasks, mech_dest, :verbose => true, :preserve => true) unless FileUtils.uptodate?(mech_dest, mech_tasks)
+
+unless File.directory?(INSTALL_DIR)
+	exit 1 unless system("#{INSTALL_CMD} -repository #{REPOS} -installIUs #{IUS}")
+end
+
+unless File.exists?(File.join(INSTALL_DIR, 'plugins', "de.guhsoft.jinto.core_#{JINTO_VERS}.jar"))
+	Dir.chdir(INSTALL_DIR)
+	FileUtils.rm_f("de.guhsoft.jinto-*.zip*")
+	exit 1 unless get_file_from_url("http://www.guh-software.de/jinto/de.guhsoft.jinto-#{JINTO_VERS}.zip")
+  exit 1 unless system("unzip de.guhsoft.jinto-#{JINTO_VERS}.zip")
+end if JINTO_VERS
+puts "#{Time.now}: finished installing #{IUS} into #{CHECKOUT}/director, #{CHECKOUT} and #{INSTALL_DIR}"
